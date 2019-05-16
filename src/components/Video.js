11bea161dadae30d00deb0acdfe928b8449349e8
Steve Frenzel <stevefrenzel@gmx.de>
2019-05-16 12:03:24 +0200

    progress week 12, day 4

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,22 +5,43 @@ import video from '../video/background_video_hi_res.mp4';
 export default class Video extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {};
         this.startVideo = this.startVideo.bind(this);
+        this.dontLoadVideo = this.dontLoadVideo.bind(this);
         this.video_loop = React.createRef();
     }
 
+    componentDidMount() {
+        this.dontLoadVideo();
+        window.addEventListener('resize', this.dontLoadVideo);
+    }
+
     startVideo() {
-        if (this.video_loop.current.paused) {
-            this.video_loop.current.play();
-        } else {
-            this.video_loop.current.pause();
+        try {
+            if (this.video_loop.current.paused) {
+                this.video_loop.current.play();
+            } else {
+                this.video_loop.current.pause();
+            }
+        } catch (e) {
+            console.log('startVideo() ERROR: ', e);
         }
     }
 
-    blurWhenScrolling() {
-        window.addEventListener('scroll', function(e) {
-            console.log('Scroll information:', e);
-        });
+    dontLoadVideo() {
+        try {
+            var getDisplay = window
+                .getComputedStyle(this.video_loop.current)
+                .getPropertyValue('display');
+        } catch (e) {
+            console.log('ERROR: ', e);
+        }
+
+        if (getDisplay === 'none') {
+            this.setState({ stopVideoLoad: true });
+        } else {
+            this.setState({ stopVideoLoad: false });
+        }
     }
 
     render() {
@@ -33,7 +54,9 @@ export default class Video extends React.Component {
                     onScroll={this.blurWhenScrolling}
                 >
                     <div className="parallax__layer parallax__layer--base">
-                        <video src={video} ref={this.video_loop} loop />
+                        {!this.state.stopVideoLoad && (
+                            <video src={video} ref={this.video_loop} loop />
+                        )}
                     </div>
                     <div className="parallax__layer parallax__layer--fore">
                         <img
@@ -49,8 +72,3 @@ export default class Video extends React.Component {
         );
     }
 }
-
-// TO DO
-
-// generate poster image randomly
-// blur out when scrolling down
--- to-do-list.md
+++ to-do-list.md
@@ -2,6 +2,8 @@ General
 
 • can't get access to Spotify API :-(
 
+• disable event handler on #bitter_logo when fixed background appears
+
 Chrome, Opera, Safari:
 
 • #bitter_logo is not visible, unless video is display: none
