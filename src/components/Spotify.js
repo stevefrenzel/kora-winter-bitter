559dd5cd19b08913ae877e8bd65e67e98b3d9670
Steve Frenzel <stevefrenzel@gmx.de>
2019-06-09 19:28:37 +0200

    Update Spotify.js

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -1,30 +1,9 @@
 import React from 'react';
-// import secrets from '../secrets.json';
 import blueht from '../images/01_blueht.jpg';
 import welk from '../images/02_welk.jpg';
 import bitter from '../images/album_artwork.jpg';
 
-// const SPOTIFY_ID = secrets.SPOTIFY_ID;
-// const SPOTIFY_SECRET = secrets.SPOTIFY_SECRET;
-
 export default class Spotify extends React.Component {
-    // state = {};
-    //
-    // componentDidMount = () => {
-    //     fetch('https://accounts.spotify.com/api/token', {
-    //         method: 'POST',
-    //         mode: 'no-cors',
-    //         body: 'grant_type=client_credentials',
-    //         headers: {
-    //             Authorization:
-    //                 'Basic ' + btoa(`${SPOTIFY_ID}:${SPOTIFY_SECRET}`),
-    //             'Content-Type': 'application/x-www-form-urlencoded'
-    //         }
-    //     }).then(data => {
-    //         console.log('Data from fetch: ', data);
-    //     });
-    // };
-
     render() {
         return (
             <React.Fragment>
