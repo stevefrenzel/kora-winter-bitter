0fa92e3d59de9a8fd3e41ce9e2dcc0ba232ff407
Steve Frenzel <stevefrenzel@gmx.de>
2019-06-10 08:06:39 +0200

    Update Band.js

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Band = () => (
+export const Band = () => (
     <React.Fragment>
         {/* GROUP 3 */}
         <div id="band_photo_component" className="parallax__group">
