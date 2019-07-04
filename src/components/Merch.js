7a4ab7b86c5a4d5a551e15104930ac418b828ac2
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-04 15:52:23 +0200

    Create Merch.js

---
+++ src/components/Merch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Merch extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Merch</h1>
+            </React.Fragment>
+        );
+    }
+}
