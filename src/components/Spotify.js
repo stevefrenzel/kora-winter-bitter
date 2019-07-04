58a14fa3eda4217d65971021335a249aa2f59a36
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-04 15:52:28 +0200

    Update Spotify.js

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -9,38 +9,35 @@ export default class Spotify extends React.Component {
             <React.Fragment>
                 {/* GROUP 4 */}
                 <div id="spotify_component" className="parallax__group">
+
                     <div className="parallax__layer parallax__layer--fore">
+
                         <div className="image_wrapper center_content">
                             <div className="album_wrapper">
                                 <img src={bitter} alt="Kora Winter - Bitter" />
                                 <p>Bitter</p>
                             </div>
+
                             <div className="album_wrapper">
-                                <a
-                                    href="https://open.spotify.com/album/1aj7iAfh1c1io39LPNi3t1?si=pTUd83UTRbGBnsKjL9Wckw"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
+                                <a href="https://open.spotify.com/album/1aj7iAfh1c1io39LPNi3t1?si=pTUd83UTRbGBnsKjL9Wckw" target="_blank" rel="noopener noreferrer">
                                     <img src={welk} alt="Kora Winter - Welk" />
                                     <p>Welk</p>
                                 </a>
                             </div>
+
                             <div className="album_wrapper">
-                                <a
-                                    href="https://open.spotify.com/album/5rXw9rHkAbbsjegFTu5PuD?si=SxrcdTwTRZ6eVKcspeiRTA"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <img
-                                        src={blueht}
-                                        alt="Kora Winter - Blueht"
-                                    />
+                                <a href="https://open.spotify.com/album/5rXw9rHkAbbsjegFTu5PuD?si=SxrcdTwTRZ6eVKcspeiRTA" target="_blank" rel="noopener noreferrer">
+                                    <img src={blueht} alt="Kora Winter - Blueht" />
                                     <p>Blüht</p>
                                 </a>
                             </div>
+
                         </div>
+
                     </div>
+
                     <div className="parallax__layer parallax__layer--base" />
+                    
                 </div>
             </React.Fragment>
         );
