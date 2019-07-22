4f59061e21b6dd62f2f28bb2ffa03007bcdd2f8f
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-22 11:18:54 +0200

    Update Merch.js

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -4,8 +4,16 @@ export default class Merch extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <h1>Merch</h1>
+                {/* GROUP 3 */}
+                <div id="merch_photo_component" className="parallax__group">
+                    <div className="parallax__layer parallax__layer--fore" />
+                    <div className="parallax__layer parallax__layer--base" />
+                </div>
             </React.Fragment>
         );
     }
 }
+
+// TO DO:
+
+// reduce file size to decrease loading time
