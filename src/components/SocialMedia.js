97ceda9293c518e304a6d25cb266e214d829408d
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-22 11:18:57 +0200

    Update SocialMedia.js

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,32 +1,38 @@
 import React from 'react';
 
-const SocialMedia = () => (
-    <React.Fragment>
-        <div id="social_media_component" className="parallax__group">
+export default class SocialMedia extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div id="social_media_component" className="parallax__group">
 
-            <div className="parallax__layer parallax__layer--back" />
+                    <div className="parallax__layer parallax__layer--back" />
 
-            <div className="parallax__layer parallax__layer--base">
+                    <div className="parallax__layer parallax__layer--base">
 
-                <div className="center_content icons">
-                    <a href="https://www.facebook.com/korawinterband" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-facebook-square" />
-                    </a>
-                    <a href="https://www.instagram.com/korawinter/" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-instagram" />
-                    </a>
-                    <a href="https://twitter.com/korawinter" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-twitter-square" />
-                    </a>
-                    <a href="https://korawinter.bandcamp.com/merch" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-bandcamp" />
-                    </a>
+                        <div className="center_content icons">
+                            <a href="https://www.facebook.com/korawinterband" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-facebook-square" />
+                            </a>
+                            <a href="https://www.instagram.com/korawinter/" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-instagram" />
+                            </a>
+                            <a href="https://twitter.com/korawinter" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-twitter-square" />
+                            </a>
+                            <a href="https://korawinter.bandcamp.com/merch" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-bandcamp" />
+                            </a>
+                        </div>
+
+                    </div>
+
                 </div>
+            </React.Fragment>
+        );
+    }
+}
 
-            </div>
-            
-        </div>
-    </React.Fragment>
-);
+// TO DO:
 
-export default SocialMedia;
+// media queries need to be fixed
