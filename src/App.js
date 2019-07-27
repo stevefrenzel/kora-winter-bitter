bdc34b0ed864ecaa72e378e99d35103c480c7729
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-27 14:26:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React from 'react';
 import Video from './components/Video';
 // import Tourdates from './components/Tourdates';
 import Band from './components/Band';
-import Spotify from './components/Spotify';
+import Music from './components/Music';
 import SocialMedia from './components/SocialMedia';
-// import LegalNotice from './components/LegalNotice';
+import LegalNotice from './components/LegalNotice';
 import Merch from './components/Merch';
 
 export default class App extends React.Component {
+    state = { show: false}
+
+    handleChange = () => {
+        if (this.state.show) {
+            console.log('show: false');
+            this.setState({ show: false });
+        } else if (!this.state.show) {
+            console.log('show: true');
+            this.setState({ show: true });
+        }
+    }
+
     render() {
         return (
                 <div className="parallax">
                     <Video />
                     <Band />
                     <Merch />
-                    <Spotify />
-                    <SocialMedia />
+                    <Music />
+                    <SocialMedia 
+                    handleChange={this.handleChange}
+                    componentVisible={this.state.show}
+                    />
+                    {this.state.show ? <LegalNotice /> : null}
                 </div>
         );
     }
