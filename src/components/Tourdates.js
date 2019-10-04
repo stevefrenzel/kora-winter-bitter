151a3a2b772261b50580a56f1cfd4a89e8082da6
jonasz <mazuba@web.de>
2019-10-04 16:06:57 +0200

    Implement tourdates.json and remove docker container

--- src/components/Tourdates.js
+++ src/components/Tourdates.js
@@ -1,52 +1,75 @@
 import React from 'react';
-// import secrets from '../secrets.json';
 
 const moment = require('moment');
 moment().format();
 
-// const API_KEY = secrets.BANDSINTOWN_ID;
-
 export default class Tourdates extends React.Component {
+
     state = {};
+
+    componentDidMount = async () => {
+        const api_call = await fetch(
+            '/tourdates.json'
+        );
+        const data = await api_call.json();
+        this.setState({
+            tourdates: data
+        });
+    };
+
     render() {
-        // if (!this.state.bandsInTown) {
-        //     return null;
-        //     // add spinner
-        // }
+        if (!this.state.tourdates) {
+            return null;
+            // add spinner
+        }
         return (
             <React.Fragment>
                 {/* GROUP 2*/}
                 <div id="tourdates_component" className="parallax__group bitter-font">
                     <div className="tourdates_content parallax__layer parallax__layer--fore">
-                       <h1>Tour Dates</h1>
-                        {/*<p className="tourdate_entry">*/}
-                        {/*    17.Aug.2019 - Eisenach - Everland Festival*/}
-                        {/*</p>*/}
-                        {/*<p className="tourdate_entry">*/}
-                        {/*    20.Aug.2019 - Berlin - Cassiopeia (Evergreen Terrace Support)*/}
-                        {/*</p>*/}
-                        {/*<p className="tourdate_entry">*/}
-                        {/*    <a href="https://www.facebook.com/events/349633399195006/" target="_blank" rel="noopener noreferrer">*/}
-                        {/*        21.Sep.2019 - Berlin - Heartcore Festival*/}
-                        {/*    </a>*/}
-                        {/*</p>*/}
-                        <p className="tourdate_entry">
-                            <a href="https://www.facebook.com/events/367179160867474/" target="_blank" rel="noopener noreferrer">
-                                05.Okt.2019 - Plochingen - JUZEPlochingen
-                            </a>
-                        </p>
-                        <p className="tourdate_entry">
-                            <a href="https://www.facebook.com/events/387345895264697/" target="_blank" rel="noopener noreferrer">
-                                18.Okt.2019 - Karlsruhe - P8 (w/ Knife Eyes)
-                            </a>
-                        </p>
-                        <p className="tourdate_entry">
-                            02.Nov.2019 - Leipzig - TBA (w/ Lessoner)
-                        </p>
+                        <h1>Tour Dates</h1>
+                        {this.state.tourdates.map(event => {
+                            // if yesterday is before tourdate
+                            if (new Date().setDate(new Date().getDate() - 1) < new Date(event.when)) {
+                                return (
+                                    <p className="tourdate_entry">
+                                        <a href={event.url} target="_blank" rel="noopener noreferrer">
+                                            {getDate(event.when)} - {event.city} - {event.where}
+                                        </a>
+                                    </p>
+                                );
+                            }
+                            return null;
+                        })}
                     </div>
-                    <div className="parallax__layer parallax__layer--base" />
+                    <div className="parallax__layer parallax__layer--base"/>
                 </div>
             </React.Fragment>
         );
     }
+}
+
+function getDate(date) {
+    let dateObj = new Date(date);
+    let germanMonth = [
+        "Jan",
+        "Feb",
+        "Mär",
+        "Apr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Okt",
+        "Nov",
+        "Dez"
+    ];
+
+    let day = dateObj.getDate() + "";
+    if (1 === day.length) {
+        day = "0" + day;
+    }
+
+    return day + "." + germanMonth[dateObj.getMonth()] + "." + dateObj.getFullYear();
 }
